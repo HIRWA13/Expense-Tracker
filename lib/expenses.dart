6ef4268f799a59afb66980d632a0c5a162ddf9be
Junior Hirwa <iamhirwejr@gmail.com>
2024-11-13 20:22:56 +0200

    feat:  add expenses screen

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,15 +1,59 @@
+import 'package:expennse_tracker/widgets/expense_list/expenses_list.dart';
+import 'package:expennse_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
-class  Expenses extends StatefulWidget {
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
   @override
   State<Expenses> createState() => _ExpensesState();
 }
 
-class  _ExpensesState extends State<Expenses> {
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter course',
+      amount: 19.69,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema  night',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) {
+          return const Text('modal overlay');
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter ExpenseTracker'),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const Text('Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
   }
 }
